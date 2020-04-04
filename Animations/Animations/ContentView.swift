f9314f7f0ae44b53bbcffa33d3c9ca6b1b4eff5b
acicartagena <acicartagena@gmail.com>
2020-04-04 21:51:05 +1100

    day 32 example 2

--- Animations/Animations/ContentView.swift
+++ Animations/Animations/ContentView.swift
@@ -9,8 +9,28 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var animationAmount: CGFloat = 1
+
     var body: some View {
-        Text("Hello, World!")
+        print(animationAmount)
+        return VStack {
+            Stepper("Scale amount", value: $animationAmount.animation(
+                Animation.easeInOut(duration: 1)
+                    .repeatCount(3, autoreverses: true)
+            ), in: 1...10)
+
+            Spacer()
+
+            Button("Tap Me") {
+                self.animationAmount += 1
+            }
+            .padding(40)
+            .background(Color.red)
+            .foregroundColor(.white)
+            .clipShape(Circle())
+            .scaleEffect(animationAmount)
+        }
     }
 }
 
