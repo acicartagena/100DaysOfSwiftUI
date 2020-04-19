d48c2cde7a520953dddf8b0f4320c9e6cd5293b9
acicartagena <acicartagena@gmail.com>
2020-04-19 14:23:28 +1000

    animation challenge
    got shake from https://www.objc.io/blog/2019/10/01/swiftui-shake-animation/

--- GuessTheFlag/GuessTheFlag/ContentView.swift
+++ GuessTheFlag/GuessTheFlag/ContentView.swift
@@ -10,12 +10,27 @@ import SwiftUI
 
 struct ContentView: View {
 
+    enum FlagAnimation {
+        case rotate
+        case fade
+    }
+
     @State private var showingScore = false
     @State private var scoreTitle = ""
     @State private var score: Int = 0
 
     @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Russia", "Spain", "UK", "US"].shuffled()
-    @State private var correctAnswer = Int.random(in: 0...2)
+    @State private var correctAnswer: Int = Int.random(in: 0...2)
+
+    @State private var animationDuration = 1.0
+
+    // correct answer
+    @State private var animationRotateDegrees = 0.0
+    @State private var animationOpacity = 1.0
+
+    // wrong answer
+    @State private var attempts: CGFloat = 0.0
+
 
     var body: some View {
         ZStack {
@@ -31,12 +46,32 @@ struct ContentView: View {
                         .fontWeight(.black)
                 }
                 ForEach(0..<3) { index in
-                    Button(action: {
-                        self.flagTapped(index)
-                    }) {
-                        Image(self.countries[index])
-                            .renderingMode(.original)
-                            .flagImage()
+                    if index == self.correctAnswer {
+                        Button(action: {
+                            self.flagTapped(index)
+                            withAnimation(.easeOut(duration: self.animationDuration)) {
+                                self.animationRotateDegrees = 360
+                                self.animationOpacity = 0.25
+                            }
+                        }) {
+                            Image(self.countries[index])
+                                .renderingMode(.original)
+                                .flagImage()
+                                .opacity(100.0)
+                        }.shake(self.attempts)
+                        .rotation3DEffect(.degrees(self.animationRotateDegrees), axis: (x: 0, y: 1, z: 0))
+                    } else {
+                        Button(action: {
+                            self.flagTapped(index)
+                            withAnimation(.default) {
+                                self.attempts += 1.0
+                                self.animationOpacity = 0.25
+                            }
+                        }) {
+                            Image(self.countries[index])
+                                .renderingMode(.original)
+                                .flagImage()
+                        }.opacity(self.animationOpacity)
                     }
                 }
                 Spacer()
@@ -56,18 +91,18 @@ struct ContentView: View {
             scoreTitle = "Wrong that's the flag of \(countries[number])"
         }
 
-        showingScore = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + self.animationDuration) {
+            self.showingScore = true
+        }
     }
 
     func askQuestion() {
+        animationRotateDegrees = 0
+        animationOpacity = 1.0
+        attempts = 0.0
+
         countries.shuffle()
         correctAnswer = Int.random(in: 0...2)
-    }
-}
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
     }
 }
 
@@ -83,5 +118,29 @@ struct FlagImage: ViewModifier {
 extension View {
     func flagImage() -> some View {
         self.modifier(FlagImage())
+    }
+}
+
+struct Shake: GeometryEffect {
+    var amount: CGFloat = 10
+    var shakesPerUnit = 3
+    var animatableData: CGFloat
+
+    func effectValue(size: CGSize) -> ProjectionTransform {
+        ProjectionTransform(CGAffineTransform(translationX:
+            amount * sin(animatableData * .pi * CGFloat(shakesPerUnit)),
+            y: 0))
+    }
+}
+
+extension View {
+    func shake(_ attempts: CGFloat) -> some View {
+        self.modifier(Shake(animatableData: attempts))
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
