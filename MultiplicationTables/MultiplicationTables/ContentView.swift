0ff9773fd60c653d795d2bf2ab783db280869c3f
acicartagena <acicartagena@gmail.com>
2020-04-19 16:08:05 +1000

    initial commit

---
+++ MultiplicationTables/MultiplicationTables/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultiplicationTables
+//
+//  Created by Angela Cartagena on 19/4/20.
+//  Copyright © 2020 ACartagena. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
