d85f10ceb0eda6b26ddb788f91fa12ed09d235ff
acicartagena <acicartagena@gmail.com>
2020-06-14 18:20:37 +1000

    Day 43 and a bit of Day 44

---
+++ Drawing/Drawing/ContentView.swift
@@ -0,0 +1,36 @@
+//
+//  ContentView.swift
+//  Drawing
+//
+//  Created by Angela Cartagena on 14/6/20.
+//  Copyright © 2020 ACartagena. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+
+    @State private var petalOffset = -20.0
+    @State private var petalWidth = 100.0
+
+    var body: some View {
+        VStack {
+            Flower(petalOffset: petalOffset, petalWidth: petalWidth)
+                .fill(Color.red, style: FillStyle(eoFill: true))
+
+            Text("Offset")
+            Slider(value: $petalOffset, in: -40...40)
+                .padding([.horizontal, .bottom])
+
+            Text("Width")
+            Slider(value: $petalWidth, in: 0...100)
+                .padding(.horizontal)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Drawing/Drawing/Triangle.swift
@@ -0,0 +1,22 @@
+//
+//  Triangle.swift
+//  Drawing
+//
+//  Created by Angela Cartagena on 14/6/20.
+//  Copyright © 2020 ACartagena. All rights reserved.
+//
+
+import SwiftUI
+
+struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+
+        return path
+    }
+}
